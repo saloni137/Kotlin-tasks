a0c72b03af34a9ffe21710b6e8c819ead84ce920
Saloni Saraiya <saloni.saraiya@dhiwise.com>
2022-04-18 16:45:24 +0530

    Create task2.kt

---
+++ task2.kt
@@ -0,0 +1,63 @@
+abstract class Beverage{
+    abstract fun cleanPot()
+    fun addWater(){
+        println("Adding hot water");
+    }
+    fun addMilk(){
+        println("Adding hot milk");
+    }
+    fun addSugar(){
+        println("Adding sugar");
+    }
+    abstract fun getMixture()
+    abstract fun addIngredients()
+    fun getBevrage(){
+        cleanPot()
+        getMixture()
+       	addIngredients()
+    }
+}
+
+class coffee : Beverage(){
+    override fun cleanPot(){
+        println("Cleaning Coffee pot...")
+    }
+    override fun getMixture(){
+        println("Your Coffee is getting ready")
+        addWater()
+        addMilk()
+        addSugar()
+    }
+    override fun addIngredients(){
+        println("Coffee Bag added");
+        println("Your coffee is ready! Enjoy")
+    }
+}
+
+class tea : Beverage(){
+    override fun cleanPot(){
+        println("Cleaning tea pot...")
+    }
+    override fun getMixture(){
+        println("Your tea is getting ready")
+        addWater()
+        addMilk()
+        addSugar()
+    }
+    override fun addIngredients(){
+        println("Tea Bag added");
+        println("Your tea is ready! Enjoy")
+    }
+}
+
+fun main(args:Array<String>) {
+   val type = args[0]
+   var beverage: Beverage?
+   if(type == "1"){
+       beverage = tea()
+   }
+   else {
+       beverage = coffee()
+   }
+   beverage.getBevrage()
+}
